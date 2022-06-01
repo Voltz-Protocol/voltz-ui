a7bfd4f8a8cd8b48ab5a25500d529eaed60261f2
Alex Davies <alexdavies@voltz.xyz>
2022-06-01 15:58:43 +0100

    fix: reload trade info if approvals change

--- src/application/App/components/LiquidityProvider/components/ConnectedMintBurnForm/ConnectedMintBurnForm.tsx
+++ src/application/App/components/LiquidityProvider/components/ConnectedMintBurnForm/ConnectedMintBurnForm.tsx
@@ -8,7 +8,7 @@ import { actions, selectors } from '@store';
 import { MintBurnFormLiquidityAction, MintBurnFormMarginAction, useAgent, useAMMContext, useBalance, useDispatch, useMintBurnForm, useSelector, useTokenApproval } from '@hooks';
 import { MintBurnForm, MintBurnFormModes, PendingTransaction } from '@components/interface';
 import { updateFixedRate } from './utilities';
-import { getFormAction, getSubmitAction, getSubmitButtonHint, getSubmitButtonText } from './services';
+import * as s from './services';
 import { isUndefined } from 'lodash';
 
 export type ConnectedMintBurnFormProps = {
@@ -39,11 +39,13 @@ const ConnectedMintBurnForm: React.FunctionComponent<ConnectedMintBurnFormProps>
 
   const [transactionId, setTransactionId] = useState<string | undefined>();
   const activeTransaction = useSelector(selectors.transactionSelector)(transactionId);
-  const formAction = getFormAction(mode, form.state.liquidityAction);
+  const formAction = s.getFormAction(mode, form.state.liquidityAction);
   const isBurningLiquidity = mode === MintBurnFormModes.EDIT_LIQUIDITY && form.state.liquidityAction === MintBurnFormLiquidityAction.BURN;
   const isRemovingMargin = mode === MintBurnFormModes.EDIT_MARGIN && form.state.marginAction === MintBurnFormMarginAction.REMOVE;
-  const submitButtonHint = getSubmitButtonHint(amm, mode, form, tokenApprovals);
-  const submitButtonText = getSubmitButtonText(mode, tokenApprovals, amm, form.state);
+  const submitButtonHint = s.getSubmitButtonHint(amm, mode, form, tokenApprovals);
+  const submitButtonText = s.getSubmitButtonText(mode, tokenApprovals, amm, form.state);
+
+  const approvalsNeeded = s.approvalsNeeded(tokenApprovals, isBurningLiquidity, isRemovingMargin);
 
   const handleComplete = () => {
     onReset();
@@ -68,14 +70,14 @@ const ConnectedMintBurnForm: React.FunctionComponent<ConnectedMintBurnFormProps>
   const handleSubmit = () => {
     if(!form.isValid) return;
 
-    if(!isBurningLiquidity && !isRemovingMargin) {
+    if(approvalsNeeded) {
       if(!tokenApprovals.underlyingTokenApprovedForPeriphery) {
         tokenApprovals.approveUnderlyingTokenForPeriphery();
         return;
       }
     }
 
-    const action = getSubmitAction(amm, formAction, form.state, agent, mode);
+    const action = s.getSubmitAction(amm, formAction, form.state, agent, mode);
     setTransactionId(action.payload.transaction.id);
     dispatch(action);
   };
@@ -93,7 +95,7 @@ const ConnectedMintBurnForm: React.FunctionComponent<ConnectedMintBurnFormProps>
         notional: form.state.notional 
       });
     }
-  }, [loadMintInfo, form.state.notional, form.state.fixedLow, form.state.fixedHigh]);
+  }, [loadMintInfo, form.state.notional, form.state.fixedLow, form.state.fixedHigh, approvalsNeeded]);
 
   if (!amm) {
     return null;
